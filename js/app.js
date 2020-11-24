222a2abf4ee3405ca3eda684014bb75a2a7d796e
Ymoth3p <67107094+Ymoth3p@users.noreply.github.com>
2020-11-24 22:36:55 +0100

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,162 @@
+const sr = ScrollReveal();
+
+
+// sr.reveal('.BTeffect', {
+//     origin: 'top',
+//     distance: '1000px',
+//     delay: 2500,
+//     duration: 1750,
+//     scale: 1,
+//     opacity: 0,
+//     easing: 'ease',
+// });
+sr.reveal('.eightythree', {
+    origin: 'right',
+    distance: '1000px',
+    delay: 3500,
+    duration: 1750,
+    scale: 1,
+    opacity: 0,
+    easing: 'ease',
+});
+
+sr.reveal('.p-brand', {
+    origin: 'top',
+    distance: '0px',
+    delay: 1200,
+    duration: 500,
+    scale: 1.3,
+    opacity: 0,
+    easing: 'ease',
+});
+sr.reveal('.sub-text1', {
+    origin: 'left',
+    distance: '200px',
+    delay: 750,
+    duration: 500,
+    scale: 1,
+    opacity: 0,
+    easing: 'ease',
+});
+sr.reveal('.sub-text2', {
+    origin: 'right',
+    distance: '200px',
+    delay: 750,
+    duration: 500,
+    scale: 1,
+    opacity: 0,
+    easing: 'ease',
+});
+sr.reveal('.sub-text3', {
+    origin: 'left',
+    distance: '200px',
+    delay: 750,
+    duration: 500,
+    scale: 1,
+    opacity: 0,
+    easing: 'ease',
+});
+
+
+
+
+
+
+
+sr.reveal('.wrapper', {
+    origin: 'left',
+    distance: '200px',
+    delay: 450,
+    duration: 1000,
+    scale: 1,
+    opacity: 0,
+    easing: 'ease',
+});
+
+sr.reveal('iframe', {
+    origin: 'right',
+    distance: '200px',
+    delay: 450,
+    duration: 1000,
+    scale: 1,
+    opacity: 0,
+    easing: 'ease',
+});
+
+// sr.reveal('footer', {
+//     origin: 'bottom',
+//     distance: '15px',
+//     delay: 300,
+//     duration: 1000,
+//     scale: 1,
+//     opacity: 0,
+//     easing: 'ease',
+// });
+
+sr.reveal('.displaycard1', {
+    origin: 'right',
+    distance: '200px',
+    delay: 600,
+    duration: 550,
+    scale: 1,
+    opacity: 0,
+    easing: 'ease',
+});
+sr.reveal('.displaycard2', {
+    origin: 'right',
+    distance: '200px',
+    delay: 850,
+    duration: 550,
+    scale: 1,
+    opacity: 0,
+    easing: 'ease',
+});
+sr.reveal('.displaycard3', {
+    origin: 'right',
+    distance: '200px',
+    delay: 1150,
+    duration: 550,
+    scale: 1,
+    opacity: 0,
+    easing: 'ease',
+});
+sr.reveal('.displaycard4', {
+    origin: 'right',
+    distance: '200px',
+    delay: 1300,
+    duration: 550,
+    scale: 1,
+    opacity: 0,
+    easing: 'ease',
+});
+sr.reveal('.displaycardinfo', {
+    origin: 'right',
+    distance: '100px',
+    delay: 1000,
+    duration: 550,
+    scale: 1,
+    opacity: 0,
+    easing: 'ease',
+});
+sr.reveal('.displaycardinfoimg', {
+    origin: 'left',
+    distance: '100px',
+    delay: 1000,
+    duration: 550,
+    scale: 1,
+    opacity: 0,
+    easing: 'ease',
+});
+
+
+
+// sr.reveal('.col', {
+//     origin: 'left',
+//     distance: '100px',
+//     delay: 1500,
+//     duration: 1000,
+//     scale: 1,
+//     opacity: 0,
+//     easing: 'ease',
+// });
+
